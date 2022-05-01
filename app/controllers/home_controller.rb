a02c2092e58ff4dcf99e04df02b125b5dcab4cdc
t-john93 <tjohnson1293@gmail.com>
2022-05-01 15:05:45 -0700

    Added user auth to friends controller, filter friends list by user, show friends list page as home page when user signed in.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,13 @@
 class HomeController < ApplicationController
+  
   def index
+    check_signed_in
   end
 
   def about
+  end
+
+  def check_signed_in
+    redirect_to friends_path if signed_in?
   end
 end
